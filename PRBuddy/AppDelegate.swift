faae3b9d8b1c4648492753ca374868ff4cd8a86b
Chris Brind <brindy@duckduckgo.com>
2018-02-23 13:04:16 +0000

    Initial Commit

---
+++ PRBuddy/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PRBuddy
+//
+//  Created by Chris Brind on 23/02/2018.
+//  Copyright © 2018 DuckDuckGo, Inc. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
